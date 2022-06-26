9708fa19c955f40304365d1958d14127b757bd16
unknown <hazine505@gmail.com>
2022-06-26 12:43:40 +0300

    modified Task2

--- Task1CountNums/Program.cs
+++ Task1CountNums/Program.cs
@@ -15,51 +15,25 @@ void PrintArray(int[] MassInt)
 {
     for (int i = 0; i < MassInt.Length; i++)
     {
-        int element = MassInt[i];
-        Console.Write($"{element}; ");
+        Console.Write($" {MassInt[i]} ");
     }
     Console.WriteLine();
 }
 
 int FindPositiveNum(int[] MassInt)
 {
+    int count = 0;
     for (int i = 0; i < MassInt.Length; i++)
     {
-        int count = 0;
+        
         if (MassInt[i] > 0)
         {
-            count = i;
-            i++;
+            count++;
         }
-        return count;
     }
+    return count;
 } 
 
 PrintArray(MassInt);
 Console.WriteLine(FindPositiveNum(MassInt));
-
-
-
-/*int[] MassInt = new int[numbers.length];
-
-for (int i = 0; i < numbers.length; i++)
-{
-    MassInt[i] = Convert.ToInt32(numbers[i]);
-}
-Console.WriteLine("{MassInt}");
-
-Console.WriteLine("Количество положительных чисел {count}");
-
-Console.Write("Введите элементы(через пробел): ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
- 
-for (int i = 0; i < arr.Length; i++)
-{
-    if (arr[i] > 0)
-    {
-        count++;
-    }
-}*/
-
 
